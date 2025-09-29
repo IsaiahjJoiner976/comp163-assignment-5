9693c4ba1ce52a9097c9ad888e3688956a475a4d
IsaiahjJoiner976 <ijjoiner@aggies.ncat.edu>
2025-09-29 13:52:31 -0400

    Challenge 1: Collatz sequence - while loop for unknown iterations

---
+++ isaiah_assignment_5.py
@@ -0,0 +1,18 @@
+step_count = 0
+
+print("=== Challenge 1: Collatz Conjecture ===")
+current_number = int(input("Enter starting number: Sequence: "))
+print(current_number, end=" ")
+while current_number > 1:
+    if current_number % 2 == 0:
+        current_number //= 2
+    else:
+        current_number = current_number * 3 + 1
+    step_count += 1
+    if current_number == 1:
+        print(current_number, end="")
+        break
+    else: 
+        print(current_number, end=" ")
+print(f"\nSteps: {step_count}")
+print("")
